1fa192071fd5ac9516706f606ac41497ef8cfac1
maxmudjon101 <149583033+maxmudjon101@users.noreply.github.com>
2024-01-13 16:33:10 +0500

    oraliq javoblar

---
+++ 12-dars.py
@@ -0,0 +1,45 @@
+# lug'tlar mavzusini oid masalalar
+ #1)  Mevalar lug’atini tuzing  va  kamida  5 ta qiymat bo’lsin
+
+mevalar={
+     "banana":"appl",
+     "ananas":"mandarin",
+     "mandarin":"qulubnika",
+     "kivi":"nok",
+     "aplisin":"anor"
+ }
+# 2)  tuzgan  Lug’atingizdan eng katta qiymatni toping
+eng_kata=max(mevalar)
+print=len(mevalar)
+
+print(eng_kata)
+
+#1) Masala: 1 dan 10 gacha bo'lgan sonlarni ekranga chiqaring.
+
+# son={1,2,3,4,5,6,7,8,9,10}
+# print(son)
+#
+#
+# 2)  Masala: 1 dan 10 gacha bo'lgan sonlarning kvadratlarini ekranga
+# for son in son:
+#     print(son ** 2)
+
+
+son=list(range(11,1))
+    for son in son:
+        if son%2==z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
